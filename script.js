91ad39d23f77978b1521beff04e3cc06c70c69ba
Nafisah-Zulfa-H <nafisahzulfahafizhah07@gmail.com>
2026-01-14 13:36:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const swiperText = new Swiper('.swiper-text', {
+    loop: true,
+
+    pagination: {
+    el: '.swiper02-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-nextbutton02',
+        prevEl: '.swiper-nextbutton02',
+    },
+});
